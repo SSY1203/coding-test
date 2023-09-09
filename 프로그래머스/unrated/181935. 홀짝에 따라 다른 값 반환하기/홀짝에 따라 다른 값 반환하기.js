cb1a63c0545db6d8de9cae351c892b83e685d007
Seonyoung <tlstjsdud566@naver.com>
2023-09-09 11:11:58 +0900

    [unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = 0;
+    
+    for(let i = 1; i<=n ; i++){
+        if(n%2===0 && i%2===0){
+            answer += i**2;
+        }if(n%2!==0 && i%2!==0){
+            answer += i;
+        }
+    }
+    return answer;
+}
