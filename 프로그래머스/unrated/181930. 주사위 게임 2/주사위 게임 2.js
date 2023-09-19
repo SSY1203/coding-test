4f4f093bf1de260222e1318446f9b098e5752fba
Seonyoung <tlstjsdud566@naver.com>
2023-09-19 23:07:30 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,13 +1,14 @@
 function solution(a, b, c) {
     var answer = 0;
+    const arr = new Set([a,b,c]);
     
-    if(a===b && b===c) {
+    if(arr.size===1) {
         answer = (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3)
-    }else if(a!==b && b!==c && c!== a){
+    }else if(arr.size===3){
         answer = a+b+c;
     }else{
         answer = (a+b+c)*(a**2+b**2+c**2)
     }
     
-    return answer;
+    return answer
 }
