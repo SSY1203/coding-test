a9c5cb73a5db71cf89293ae40a7d84c1e56eb1d6
Seonyoung <tlstjsdud566@naver.com>
2023-09-20 20:43:51 +0900

    [unrated] Title: 특별한 이차원 배열 1, Time: 0.19 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,15 +1,8 @@
 function solution(n) {
-    const  arr = new Array(n);
+    const arr = Array.from(new Array(n), ()=> Array(n).fill(0))
     
-    for(let i =0 ; i< arr.length;i++){
-        arr[i] = new Array(n);
-    }
-    
-    for(let j=0 ;j<n;j++){
-        for(let k=0; k<n;k++){
-            if(j===k) arr[j][k] = 1;
-            else arr[j][k] = 0;
-        }
+    for(let i =0 ;i< n;i++){
+        arr[i][i]=1;
     }
     return arr;
 }
