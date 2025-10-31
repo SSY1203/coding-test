4077050a45abc0dbfeb571f8b2987ba03d3312c4
SSY1203 <tlstjsdud566@naver.com>
2025-10-31 17:17:19 +0900

    진행중

--- baekjoon/#11653.py
+++ baekjoon/#11653.py
@@ -3,5 +3,17 @@ n = int(input())
 answers = []
 
 divider = 2
+value = n
 
-while(divider )
+# 소수 판별 함수
+def isFindPrime(number):
+    for i in range(2, int(number**0.5) + 1):
+        if(number % i == 0):
+            return False
+    return True
+        
+for i in range(2, n + 1):
+    if(value % divider == 0):
+        value = int(value / divider)
+    else:
+        
