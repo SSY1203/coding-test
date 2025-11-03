d53787e2261796cfcc2088a3bbc912c2ec436003
SSY1203 <tlstjsdud566@naver.com>
2025-11-03 10:20:06 +0900

    #11653 문제 해결

--- baekjoon/#11653.py
+++ baekjoon/#11653.py
@@ -1,19 +1,12 @@
 n = int(input())
 
-answers = []
-
 divider = 2
-value = n
 
-# 소수 판별 함수
-def isFindPrime(number):
-    for i in range(2, int(number**0.5) + 1):
-        if(number % i == 0):
-            return False
-    return True
-        
-for i in range(2, n + 1):
-    if(value % divider == 0):
-        value = int(value / divider)
+while n > 1:
+    if n % divider == 0:
+        print(divider)
+        n = n // divider
     else:
-        
+        divider += 1
+        if divider > int(n**0.5):
+            divider = n
