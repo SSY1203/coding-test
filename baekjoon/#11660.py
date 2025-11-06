cdc4b1c77c644ea3814e1c6ca3d02c29aeed613d
SSY1203 <tlstjsdud566@naver.com>
2025-11-06 21:52:39 +0900

    #11660 문제 해결

---
+++ baekjoon/#11660.py
@@ -0,0 +1,24 @@
+N, M = map(int, input().split())
+
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+positions = [list(map(int, input().split())) for _ in range(M)]
+
+prefix = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+
+
+for x in range(N):
+    for y in range(N):
+        prefix[x + 1][y + 1] = (
+            prefix[x + 1][y] + prefix[x][y + 1] - prefix[x][y] + graph[x][y]
+        )
+
+
+for i in range(M):
+    x1, y1, x2, y2 = positions[i]
+    print(
+        prefix[x2][y2]
+        - prefix[x2][y1 - 1]
+        - prefix[x1 - 1][y2]
+        + prefix[x1 - 1][y1 - 1]
+    )
