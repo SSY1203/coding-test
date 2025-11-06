ffa344a3b4e7af1e6ddd16e20a192103ca145822
SSY1203 <tlstjsdud566@naver.com>
2025-11-06 11:49:20 +0900

    #14719 문제 해결

---
+++ baekjoon/#14719.py
@@ -0,0 +1,29 @@
+h, w = map(int, input().split())
+
+blocks = list(map(int, input().split()))
+
+standard = max(blocks)
+standardIndex = blocks.index(standard)
+
+sum = 0
+
+lMax = 0
+rMax = 0
+
+# 좌측
+for i in range(standardIndex):
+    curH = blocks[i]
+
+    if curH >= lMax:
+        lMax = curH
+    sum += lMax - curH
+
+# 우측
+for j in range(w - 1, standardIndex, -1):
+    curH = blocks[j]
+
+    if curH >= rMax:
+        rMax = curH
+    sum += rMax - curH
+
+print(sum)
